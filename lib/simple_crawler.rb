3b8623ede69b09a7d8da26eb0d52a30b403dcfde
Callum Jones <callum@callumj.com>
2014-06-29 19:07:17 +0800

    Load up

--- lib/simple_crawler.rb
+++ lib/simple_crawler.rb
@@ -1,1 +1,3 @@
+require 'simple_crawler/models'
 require 'simple_crawler/downloader'
+require 'simple_crawler/scrapers'
---
+++ lib/simple_crawler/scrapers.rb
@@ -0,0 +1,9 @@
+module SimpleCrawler
+  module Scrapers
+    require 'simple_crawler/scrapers/html'
+
+    def self.for(dl_req)
+
+    end
+  end
+end
