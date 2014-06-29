570ebc249c6bb68da12c5ebba274ea7d3a0b7f46
Callum Jones <callum@callumj.com>
2014-06-29 23:59:12 +0800

    Test link exposure

--- lib/simple_crawler/scrapers/html.rb
+++ lib/simple_crawler/scrapers/html.rb
@@ -6,6 +6,9 @@ module SimpleCrawler
 
       attr_accessor :dl_resp, :document
 
+      SUPPORTED_LINK_ASSETS = %w(stylesheet icon)
+      SUPPORTED_LINK_LINKS = %w(canonical alternate external help archives)
+
       def initialize(dl_resp)
         raise ArgumentError unless dl_resp.is_a?(Models::DownloadResponse)
         @dl_resp  = dl_resp
@@ -13,9 +16,23 @@ module SimpleCrawler
       end
 
       def links
-        @links ||=  begin
-          node_set = document.xpath("//*[@href]").map do |node|
-            [node["href"], node.text]
+        @links ||= begin
+          document.xpath("//a[@href]|//link[@href]").select do |node|
+            next true if node["rel"].nil?
+            SUPPORTED_LINK_LINKS.include?(node["rel"])
+          end.map do |node|
+            [node["href"], node.text.strip]
+          end.uniq { |(href, text)| href }
+        end
+      end
+
+      def assets
+        @assets ||= begin
+          document.xpath("//link[@href]|//img[@src]|//script[@src]").select do |node|
+            next true if node["rel"].nil?
+            SUPPORTED_LINK_ASSETS.include?(node["rel"])
+          end.map do |node|
+            [node["href"] || node["src"], node.text.strip]
           end.uniq { |(href, text)| href }
         end
       end
---
+++ spec/unit/simple_crawler/scrapers/html_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe SimpleCrawler::Scrapers::HTML do
+
+  let(:download_response) do
+    SimpleCrawler::Models::DownloadResponse.new("SOURCE_CODE")
+  end
+
+  let(:nokogiri_doc) { double(:nokogiri_doc) }
+
+  before :each do
+    expect(Nokogiri).to receive(:HTML).with("SOURCE_CODE").and_return(nokogiri_doc)
+  end
+
+  subject { described_class.new download_response }
+
+  describe "#links" do
+
+    let(:nd1) do
+      double(:nd1).tap do |n|
+        expect(n).to receive(:[]).with("rel").and_return(nil)
+        expect(n).to receive(:[]).with("href").and_return("/a_href")
+        expect(n).to receive(:text).and_return("A")
+      end
+    end
+    let(:nd2) do
+      double(:nd2).tap do |n|
+        expect(n).to receive(:[]).with("rel").and_return(nil)
+        expect(n).to receive(:[]).with("href").and_return("http://google.com/a_href")
+        expect(n).to receive(:text).and_return("B")
+      end
+    end
+    let(:nd3) do
+      double(:nd3).tap do |n|
+        expect(n).to receive(:[]).with("rel").and_return("stylesheet").twice
+      end
+    end
+    let(:nd4) do
+      double(:nd4).tap do |n|
+        expect(n).to receive(:[]).with("rel").and_return("canonical").twice
+        expect(n).to receive(:[]).with("href").and_return("http://g00gle.com/a_href")
+        expect(n).to receive(:text).and_return("D")
+      end
+    end
+    let(:nd5) do
+      double(:nd5).tap do |n|
+        expect(n).to receive(:[]).with("rel").and_return("external").twice
+        expect(n).to receive(:[]).with("href").and_return("http://giggle.com/a_href")
+        expect(n).to receive(:text).and_return("E            \r\n")
+      end
+    end
+
+    before :each do
+      expect(nokogiri_doc).to receive(:xpath).with("//a[@href]|//link[@href]")
+      .and_return([nd1, nd2, nd3, nd4, nd5])
+    end
+
+    it "should be filtered correctly" do
+      expect(subject.links.length).to eq(4)
+    end
+
+    it "should include regular a hrefs" do
+      expect(subject.links).to include ["/a_href", "A"], ["http://google.com/a_href", "B"]
+    end
+
+    it "should include links" do
+      expect(subject.links).to include ["http://g00gle.com/a_href", "D"], ["http://giggle.com/a_href", "E"]
+    end
+
+  end
+
+end
