7aa68d9208dc03378e6a96257e11d73c0e4cdd4f
Callum Jones <callum@callumj.com>
2014-07-03 16:20:38 +0800

    Adding multi worker support

--- lib/simple_crawler/tasks.rb
+++ lib/simple_crawler/tasks.rb
@@ -1,1 +1,2 @@
 require 'simple_crawler/tasks/single_run'
+require 'simple_crawler/tasks/multi_worker'
---
+++ lib/simple_crawler/tasks/multi_worker.rb
@@ -0,0 +1,58 @@
+require 'addressable/uri'
+
+module SimpleCrawler
+  module Tasks
+    class MultiWorker
+
+      class Supervisor
+
+        MAX_WORKERS = 50
+
+        attr_accessor :session, :main_thread
+
+        def initialize(session)
+          @session = session
+          @active_workers = []
+        end
+
+        def run
+          @main_thread = Thread.new { self.keep_alive }
+        end
+
+        def keep_alive
+          while session.queue.peek != nil || @active_workers.any? { |t| good_worker?(t) }
+            clean_dead_workers
+            if session.queue.peek != nil
+              workers_to_start = MAX_WORKERS - @active_workers.length
+              workers_to_start.times do |i|
+                STDOUT.puts "Spawning worker #{i}"
+                @active_workers << Thread.new { Worker.new.perform(session) }
+              end
+            end
+
+            sleep 0.001
+          end
+
+          STDOUT.puts "Crawl completed. Dumping results"
+          session.dump_results
+        end
+
+        def clean_dead_workers
+          @active_workers.select! { |t| good_worker?(t) }
+        end
+
+        def good_worker?(t)
+          t.status == "sleep" || t.status == "run"
+        end
+
+      end
+
+      def self.run(initial_url, output_file)
+        session = CrawlSession.new initial_url: initial_url, output_file: output_file
+        s = Supervisor.new session
+        s.run
+        s
+      end
+    end
+  end
+end
