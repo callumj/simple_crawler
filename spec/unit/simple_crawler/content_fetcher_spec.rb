a085d183dd34536223585cc48d455a27e23204db
Callum Jones <callum@callumj.com>
2014-07-03 15:46:15 +0800

    Ensuring new changes work

--- spec/unit/simple_crawler/content_fetcher_spec.rb
+++ spec/unit/simple_crawler/content_fetcher_spec.rb
@@ -13,7 +13,9 @@ describe SimpleCrawler::ContentFetcher do
   subject { described_class.new url, session }
 
   before :each do
-    allow(SimpleCrawler::Downloader).to receive(:source_for).with(Addressable::URI.parse(url)).and_return(dl)
+    dl_uri = double(:dl)
+    allow(session).to receive(:absolute_uri_to).and_return(dl_uri)
+    allow(SimpleCrawler::Downloader).to receive(:source_for).with(dl_uri).and_return(dl)
   end
 
   describe "#merge_uri_with_page" do
@@ -85,11 +87,21 @@ describe SimpleCrawler::ContentFetcher do
         assets << ["/image.png", "Some Image", "image"]
         assets << ["http://anz.com/thingo.css", "Some stylesheet", "stylesheet"]
         assets << ["c/d/magic.gif", "Some Gif", "giffy"]
+        assets << ["DO NOT BREAK", "Some Gif", "giffy"]
+
+        a1 = Addressable::URI.parse("a")
+        a2 = Addressable::URI.parse("b")
+        a3 = Addressable::URI.parse("c")
+
+        expect(subject).to receive(:merge_uri_with_page).with(Addressable::URI.parse(assets[0][0])).and_return(a1)
+        expect(subject).to receive(:merge_uri_with_page).with(Addressable::URI.parse(assets[1][0])).and_return(a2)
+        expect(subject).to receive(:merge_uri_with_page).with(Addressable::URI.parse(assets[2][0])).and_return(a3)
+        expect(subject).to receive(:merge_uri_with_page).with(Addressable::URI.parse(assets[3][0])).and_return(nil)
 
         expect(subject.content_info.assets.to_a).to match_array([
-          SimpleCrawler::Models::Asset.new(Addressable::URI.parse("http://callumj.com/image.png"), "image"),
-          SimpleCrawler::Models::Asset.new(Addressable::URI.parse("http://anz.com/thingo.css"), "stylesheet"),
-          SimpleCrawler::Models::Asset.new(Addressable::URI.parse("http://callumj.com/a/b/c/d/magic.gif"), "giffy")
+          SimpleCrawler::Models::Asset.new(a1, "image"),
+          SimpleCrawler::Models::Asset.new(a2, "stylesheet"),
+          SimpleCrawler::Models::Asset.new(a3, "giffy")
         ])
       end
 
@@ -97,12 +109,41 @@ describe SimpleCrawler::ContentFetcher do
         links << ["/image.png", "Some Image"]
         links << ["http://anz.com/thingo.css", "Some stylesheet"]
         links << ["c/d/magic.gif", "Some Gif"]
+        links << ["NOTEVENHERE", "Some Gif"]
+
+        l1 = Addressable::URI.parse("a")
+        l2 = Addressable::URI.parse("b")
+        l3 = Addressable::URI.parse("c")
+
+        expect(subject).to receive(:merge_uri_with_page).with(Addressable::URI.parse(links[0][0])).and_return(l1)
+        expect(subject).to receive(:merge_uri_with_page).with(Addressable::URI.parse(links[1][0])).and_return(l2)
+        expect(subject).to receive(:merge_uri_with_page).with(Addressable::URI.parse(links[2][0])).and_return(l3)
+        expect(subject).to receive(:merge_uri_with_page).with(Addressable::URI.parse(links[3][0])).and_return(nil)
 
         expect(subject.content_info.links).to match_array([
-          SimpleCrawler::Models::Link.new(Addressable::URI.parse("http://callumj.com/image.png")),
-          SimpleCrawler::Models::Link.new(Addressable::URI.parse("http://anz.com/thingo.css")),
-          SimpleCrawler::Models::Link.new(Addressable::URI.parse("http://callumj.com/a/b/c/d/magic.gif"))
+          SimpleCrawler::Models::Link.new(l1),
+          SimpleCrawler::Models::Link.new(l2),
+          SimpleCrawler::Models::Link.new(l3)
         ])
+      end
+
+      it "should be resilient against broken assets or links" do
+        links << ["http:", "Some Link"]
+        assets << ["http:", "Some Image", "img"]
+
+        expect(subject.content_info.links).to be_empty
+        expect(subject.content_info.assets).to be_empty
+      end
+
+      it "should provide a relative URI in relation to the session" do
+        uri = double(:uri).tap do |u|
+          expect(u).to receive(:relative?).and_return(true)
+          expect(u).to receive(:path).and_return("path")
+        end
+        expect(session).to receive(:relative_to).with(Addressable::URI.parse("http://callumj.com/a/b/index.html")).and_return(uri)
+
+        inst = subject.content_info
+        expect(inst.final_uri).to eq uri
       end
     end
 
