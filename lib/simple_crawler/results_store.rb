ef1047d17d7e3764f21797482b94c7359306a6c4
Callum Jones <callum@callumj.com>
2014-07-03 11:00:41 +0800

    Starting tests on store

--- lib/simple_crawler/results_store.rb
+++ lib/simple_crawler/results_store.rb
@@ -8,19 +8,50 @@ module SimpleCrawler
     def initialize(opts = {})
       @crawl_session = opts[:crawl_session]
       raise ArgumentError, "A CrawlSession is required!" unless @crawl_session.is_a?(CrawlSession)
+
       @contents = Set.new
+
+      @assets_usage = Hash.new { |hash, key| hash[key] = Set.new }
+      @incoming_links = Hash.new { |hash, key| hash[key] = Set.new }
     end
 
     def add_content(content_info)
       @contents << content_info
+      content_info.assets.each { |asset| @assets_usage[asset.uri] << content_info.final_uri }
+      content_info.links.each { |link| @incoming_links[link.uri] << content_info.final_uri }
     end
 
-    def dump(file_path)
-      results = JSON.generate @contents.to_a
-      File.open(file_path, "w+") do |f|
-        f.write results
+    def dump(base_file_path)
+      prefix = if base_file_path.end_with?("/")
+        base_file_path
+      else
+        "#{base_file_path}_"
       end
+
+      map_file = "#{prefix}map.json"
+      generate_json_to_file @contents.to_a, map_file
+
+      assets_file = "#{prefix}assets.json"
+      generate_json_to_file generate_json_format(@assets_usage), assets_file
+
+      links_file = "#{prefix}incoming_links.json"
+      generate_json_to_file generate_json_format(@incoming_links), links_file
     end
+
+    private
+
+      def generate_json_to_file(contents, file)
+        results = JSON.generate contents
+        File.open(file, "w") do |f|
+          f.write results
+        end
+      end
+
+      def generate_json_format(hsh)
+        hsh.sort_by { |(uri, set)| -set.length }.each_with_object({}) do |(uri, set), hash|
+          hash[uri.to_s] = set.map(&:to_s)
+        end
+      end
 
   end
 end
---
+++ spec/unit/simple_crawler/results_store_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe SimpleCrawler::ResultsStore do
+
+  let(:session) { SimpleCrawler::CrawlSession.new }
+  subject { described_class.new crawl_session: session }
+
+  describe "initializing" do
+
+    it "should require a crawl_session" do
+      expect do
+        described_class.new thing: true
+      end.to raise_error(ArgumentError, "A CrawlSession is required!")
+    end
+
+
+    it "should accept a crawl_session" do
+      expect(subject.crawl_session).to eq(session)
+      expect(subject.contents).to be_a(Set)
+    end
+
+  end
+
+  describe "#add_content" do
+
+    let(:uri) { Addressable::URI.parse "http://enron.com/ethics" }
+    let(:content_info) { SimpleCrawler::Models::ContentInfo.new uri }
+
+    it "should append the content info to the set" do
+      expect do
+        subject.add_content content_info
+      end.to change { subject.contents.include?(content_info) }.from(false).to(true)
+    end
+
+    it "should enumerate the assets" do
+      expect(content_info).to receive(:assets).and_return([])
+      subject.add_content content_info
+    end
+
+    it "should enumerate the links" do
+      expect(content_info).to receive(:links).and_return([])
+      subject.add_content content_info
+    end
+  end
+
+end
